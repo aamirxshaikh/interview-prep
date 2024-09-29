3780ac26fde645cc04e7b2ad4ef04a69f0306bcc
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-09-29 21:23:51 +0530

    Fix comment indentation

--- core-java/src/main/java/com/interviewprep/oop_concepts/class_and_object/encapsulation/immutable_classes/Main.java
+++ core-java/src/main/java/com/interviewprep/oop_concepts/class_and_object/encapsulation/immutable_classes/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-// Mutable list of friends
+    // Mutable list of friends
     List<String> friendsList = new ArrayList<>();
     friendsList.add("Alice");
     friendsList.add("Bob");
