1ef9dd263f7a64dfd31ef732d3f591dfbd20dead
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-12-12 21:50:45 +0530

    Modify java_collections_framework/generics/bounded_types_generics/BoundedTypesExample.java

--- core-java/src/main/java/com/interviewprep/java_collections_framework/generics/bounded_types_generics/BoundedTypesExample.java
+++ core-java/src/main/java/com/interviewprep/java_collections_framework/generics/bounded_types_generics/BoundedTypesExample.java
@@ -15,6 +15,20 @@ class Bound<T extends Number> {
 public class BoundedTypesExample {
   public static void main(String[] args) {
     Bound<Integer> boundInt = new Bound<>(5);
-    System.out.println("Data: " + boundInt.getData());
+    Bound<Float> boundFloat = new Bound<>(3.14f);
+    Bound<Long> boundLong = new Bound<>(1000000000L);
+
+    System.out.println("Integer: " + boundInt.getData());
+    System.out.println("Float: " + boundFloat.getData());
+    System.out.println("Long: " + boundLong.getData());
+
+    System.out.println("Sum of integers: " + sum(10, 20));
+    System.out.println("Sum of doubles: " + sum(5.5, 4.5));
+    System.out.println("Sum of floats: " + sum(3.0f, 7.5f));
+  }
+
+  // Method that accepts only numbers or subclasses of Number
+  public static <T extends Number> double sum(T num1, T num2) {
+    return num1.doubleValue() + num2.doubleValue();
   }
 }
