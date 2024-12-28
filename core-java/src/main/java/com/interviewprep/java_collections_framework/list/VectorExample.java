8bc6007222e5d21856f3884bbe492fb088f298e5
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-12-28 13:29:38 +0530

    Add usage to threads in core-java/src/main/java/com/interviewprep/java_collections_framework/list/VectorExample.java

--- core-java/src/main/java/com/interviewprep/java_collections_framework/list/VectorExample.java
+++ core-java/src/main/java/com/interviewprep/java_collections_framework/list/VectorExample.java
@@ -1,5 +1,6 @@
 package com.interviewprep.java_collections_framework.list;
 
+import java.util.List;
 import java.util.Vector;
 
 public class VectorExample {
@@ -30,5 +31,31 @@ public class VectorExample {
     for (Integer number : vector) {
       System.out.println(number);
     }
+
+    Vector<Integer> list = new Vector<>();
+
+    Thread t1 = new Thread(() -> {
+      for (int i = 0; i < 1000; i++) {
+        list.add(i);
+      }
+    });
+
+    Thread t2 = new Thread(() -> {
+      for (int i = 0; i < 1000; i++) {
+        list.add(i);
+      }
+    });
+
+    t1.start();
+    t2.start();
+
+    try {
+      t1.join();
+      t2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Size of the list: " + list.size());
   }
 }
