05a5056bdcf5c07deca4be0323eb9fe7b8720bf4
aamirxshaikh <aamirshaikh3232@gmail.com>
2025-03-06 23:02:19 +0530

    Update thread name comment in core-java/src/main/java/com/interviewprep/concurrency/thread/lifecycle/states/blocked/Main.java

--- core-java/src/main/java/com/interviewprep/concurrency/thread/lifecycle/states/blocked/Main.java
+++ core-java/src/main/java/com/interviewprep/concurrency/thread/lifecycle/states/blocked/Main.java
@@ -8,10 +8,10 @@ public class Main {
     Thread t2 = new Thread(resource::access);
 
     t1.start();
-    Thread.sleep(100); // Ensure Thread-1 gets the lock first
+    Thread.sleep(100); // Ensure Thread-0 gets the lock first
     t2.start();
 
-    Thread.sleep(100); // Allow time for Thread-2 to be blocked
+    Thread.sleep(100); // Allow time for Thread-1 to be blocked
     System.out.println(t2.getState()); // BLOCKED if t1 is holding the lock
   }
 }
